db7c987f74fa2801e7d87743f7a0efb2a6f0aef1
shr-23-uti <vshrutivishwakarmas@gmail.com>
2025-01-09 00:58:15 +0530

    Average

---
+++ Average.java
@@ -0,0 +1,17 @@
+// to calculate average
+
+import java.util.Scanner;
+public class Average
+{
+public static void main(String[] args)
+{
+Scanner sc = new Scanner(System.in);
+System.out.println("Enter three numbers: ");
+double a = sc.nextDouble();
+double b = sc.nextDouble();
+double c = sc.nextDouble();
+
+double average = (a+b+c)/3;
+System.out.println("Average "+ average);
+}
+}
